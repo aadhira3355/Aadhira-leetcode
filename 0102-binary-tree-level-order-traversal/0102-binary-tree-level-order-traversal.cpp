91bce1a0e2e073fd6570e9dc22318567955bd342
Aadhira <127594484+aadhira3355@users.noreply.github.com>
2024-07-22 22:55:24 +0530

    Time: 3 ms (82.06%), Space: 15.5 MB (5.06%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -6,30 +6,48 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        queue<TreeNode*>q;
-        vector<vector<int>>res;
-        if (root == nullptr) return res;
-        q.push(root);
-        while(!q.empty()){
-            int n=q.size();
-            vector<int>lvl;
-            for(int i=0;i<n;i++){
-            TreeNode* node = q.front();
-                    q.pop();
-            lvl.push_back(node->val);
-            if(node->left)
-                q.push(node->left);
-            if(node->right)
-                q.push(node->right);
-            }
-            res.push_back(lvl);
+        vector<vector<int>> v;
+        if (root == NULL) {
+            return v;
         }
-        return res;
+        vector<int> v1;
+        unordered_map<TreeNode*, int> mp;
+        mp[root] = 1;
+        queue<TreeNode*> q;
+        q.push(root);
+        v1.push_back(root->val);
+        while (!q.empty()) {
+            TreeNode* ptr = q.front();
+            q.pop();
+            if (mp.find(ptr) != mp.end()) {
+                v.push_back(v1);
+                v1.clear();
+            }
+            if (ptr->left) {
+                if (v1.empty()) {
+                    mp[ptr->left] = 1;
+                }
+                v1.push_back(ptr->left->val);
+                q.push(ptr->left);
+            }
+            if (ptr->right) {
+                if (v1.empty()) {
+                    mp[ptr->right] = 1;
+                }
+                v1.push_back(ptr->right->val);
+                q.push(ptr->right);
+            }
+        }
+        for (auto& i : v1) {
+            cout << i << endl;
+        }
+        return v;
     }
 };
